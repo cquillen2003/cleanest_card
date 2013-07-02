cc59deca38be97a416d0f381be48cc0b5da173fa
Corey Quillen <corey.quillen@gmail.com>
2013-07-01 19:44:25 -0500

    fixed show/hide footer bug without ajax false work-around

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -8,48 +8,42 @@
 	//$('#footer-controls').toggle();
 //});
 
-$(document).delegate("#application-page", "pageshow", function() {
-  //alert('A page with an id of "aboutPage" was just created by jQuery Mobile!');
+$(document).on('change', '.ui-page-active .task-input', function() {
   
-  $(".task-input").change(function() {
-  	//alert("changed!");
-  	var n = $( ".task-input:checked" ).length;
-  	
-  	if (n > 1) {
-  		$('#footer-controls-1').hide();
-  		$('#footer-controls-2').show();
-  	}
-  	else if (n == 1) {
-  		$('#footer-controls-2').hide();
-  		$('#footer-controls-1').show();
-  	}  	
-  	else {
-  		$('#footer-controls-1').hide();
-  		$('#footer-controls-2').hide();
-  	}
-  });
+	var n = $( ".ui-page-active .task-input:checked" ).length;
+	
+	if (n > 1) {
+		$('.ui-page-active #footer-controls-1').hide();
+		$('.ui-page-active #footer-controls-2').show();
+	}
+	else if (n == 1) {
+		$('.ui-page-active #footer-controls-2').hide();
+		$('.ui-page-active #footer-controls-1').show();
+	}  	
+	else {
+		$('.ui-page-active #footer-controls-1').hide();
+		$('.ui-page-active #footer-controls-2').hide();
+	}
   
 });
-
-
 
 
 //TODO: Refactor 3 functions below into 1
-$(document).on('click', '#mark-task-planned', function () {
+$(document).on('click', '.ui-page-active #mark-task-planned', function () {
 	//alert("why do they make this so hard?");
-	$("#task-status").val("planned");
-	$("#mass-update-form").submit();
+	$(".ui-page-active #task-status").val("planned");
+	$(".ui-page-active #mass-update-form").submit();
 });
 
-$(document).on('click', '#mark-task-started', function () {
+$(document).on('click', '.ui-page-active #mark-task-started', function () {
 	//alert("why do they make this so hard?");
-	$("#task-status").val("started");
-	$("#mass-update-form").submit();
+	$(".ui-page-active #task-status").val("started");
+	$(".ui-page-active #mass-update-form").submit();
 });
 
-$(document).on('click', '#mark-task-done', function () {
+$(document).on('click', '.ui-page-active #mark-task-done', function () {
 	//alert("why do they make this so hard?");
-	$("#task-status").val("done");
-	$("#mass-update-form").submit();
+	$(".ui-page-active #task-status").val("done");
+	$(".ui-page-active #mass-update-form").submit();
 });
 
