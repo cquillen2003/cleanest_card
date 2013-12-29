fd10a7ce48993c117d57889da51d372013465712
Corey Quillen <corey.quillen@gmail.com>
2013-12-29 17:05:30 -0600

    add current board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,9 +2,23 @@ class BoardsController < ApplicationController
 	
   def plan 	
     #input bcat, kcat, view
-    @bcat = params[:bcat]
-    @kcat = params[:kcat]
-    @view = params[:view]
+    if params[:bcat]
+      @bcat = params[:bcat]
+    else
+      @bcat = current_user.categories.first
+    end
+
+    if params[:kcat]
+      @kcat = params[:kcat]
+    else
+      @kcat = current_user.categories.first
+    end
+
+    if params[:view]
+      @view = params[:view]
+    else
+      @view = "projects"
+    end
 
     #output @bcat, @kcat, @view, @bcards, @kcards, @categories
     @categories = current_user.categories
@@ -31,6 +45,37 @@ class BoardsController < ApplicationController
   end
 
   def current
+
+    #input cat, view
+    if params[:cat]
+      @cat = params[:cat]
+    else
+      @cat = current_user.categories.first
+    end
+
+    if params[:view]
+      @view = params[:view]
+    else
+      @view = "projects"
+    end
+
+    #output @cat, @view, @cards, @categories
+    @categories = current_user.categories
+
+    #cards
+    if @view == "projects"
+      category_cards = Category.find(@cat).projects
+      assigned_cards = current_user.projects
+      cards = category_cards + assigned_cards
+      @cards = cards.uniq
+    end
+    if @view == "tasks"
+      category_cards = Category.find(@cat).tasks
+      assigned_cards = current_user.tasks
+      cards = category_cards + assigned_cards
+      @cards = cards.uniq
+    end
+
   end
   
 end
