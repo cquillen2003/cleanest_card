ac02978f840e4eb35c280ecf4deaa8010a745e2e
Corey Quillen <corey.quillen@gmail.com>
2014-01-07 22:59:45 -0600

    remove project/task dropdown from plan board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -14,64 +14,32 @@ class BoardsController < ApplicationController
       @kcat = current_user.categories.first
     end
 
-    if params[:view]
-      @view = params[:view]
-    else
-      @view = "projects"
-    end
-
     #output @bcat, @kcat, @view, @bcards, @kcards, @categories
     @categories = current_user.categories
     
-    #backlog
+    #backlog cards
     backlog_projects = Project.backlog(current_user.id, @bcat)
-    backlog_tasks = Category.find(@bcat).tasks.where({ :status => "backlog" })
-    @backlog_cards = backlog_projects + backlog_tasks
-
+    backlog_category_tasks = Category.find(@bcat).tasks.where({ :status => "backlog" })
+    backlog_assigned_tasks = current_user.tasks.where({ :status => "backlog" })
+    @backlog_cards = backlog_projects + backlog_category_tasks + backlog_assigned_tasks
+    
+    #planned cards
     planned_projects = Project.planned(current_user.id, @kcat)
+    planned_category_tasks = Category.find(@kcat).tasks.where({ :status => "planned" })
+    planned_assigned_tasks = current_user.tasks.where({ :status => "planned" })
+    @planned_cards = planned_projects + planned_category_tasks + planned_assigned_tasks
+
+    #started tasks
     started_projects = Project.started(current_user.id, @kcat)
-    done_projects = Project.done(current_user.id, @kcat) 
+    started_category_tasks = Category.find(@kcat).tasks.where({ :status => "started" })
+    started_assigned_tasks = current_user.tasks.where({ :status => "started" })
+    @started_cards = started_projects + started_category_tasks + started_assigned_tasks
 
-    #kanban board
-    if @view == "projects"
-      @planned_cards = planned_projects
-      @started_cards = started_projects
-      @done_cards = done_projects
-    end
-    if @view == "tasks"
-      #planned tasks
-      planned_project_tasks = []
-      planned_projects.each do |project|
-        planned_project_tasks = planned_project_tasks + project.tasks.where({ :status => "planned" })
-      end
-
-      planned_category_tasks = Category.find(@kcat).tasks.where({ :status => "planned" })
-      planned_assigned_tasks = current_user.tasks.where({ :status => "planned" })
-
-      @planned_cards = planned_project_tasks + planned_category_tasks + planned_assigned_tasks
-
-      #started tasks
-      started_project_tasks = []
-      started_projects.each do |project|
-        started_project_tasks = started_project_tasks + project.tasks.where({ :status => "started" })
-      end
-
-      started_category_tasks = Category.find(@kcat).tasks.where({ :status => "started" })
-      started_assigned_tasks = current_user.tasks.where({ :status => "started" })
-
-      @started_cards = started_project_tasks + started_category_tasks + started_assigned_tasks
-
-      #done tasks      
-      done_project_tasks = []
-      done_projects.each do |project|
-        done_project_tasks = done_project_tasks + project.tasks.where({ :status => "done" })
-      end
-
-      done_category_tasks = Category.find(@kcat).tasks.where({ :status => "done" })
-      done_assigned_tasks = current_user.tasks.where({ :status => "done" })
-
-     	@done_cards = done_project_tasks + done_category_tasks + done_assigned_tasks
-    end 
+    #done tasks
+    done_projects = Project.done(current_user.id, @kcat)      
+    done_category_tasks = Category.find(@kcat).tasks.where({ :status => "done" })
+    done_assigned_tasks = current_user.tasks.where({ :status => "done" })
+   	@done_cards = done_projects + done_category_tasks + done_assigned_tasks
 
   end
 
