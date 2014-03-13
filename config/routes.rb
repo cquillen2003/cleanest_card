1a60da5817037107ccb3b1674e3eab99aaa90b5d
Corey Quillen <corey.quillen@gmail.com>
2014-03-13 00:37:28 -0500

    start adding angularjs router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CleanestCard::Application.routes.draw do
 	
-	root :to => 'sessions#new'
+	#root :to => 'sessions#new'
 	
   get "boards/plan", :as => :plan
 
--- public/angularjs_client/js/app.js
+++ public/angularjs_client/js/app.js
@@ -1,1 +1,22 @@
-var cleanCard = angular.module('cleanCard', ['cleanCardControllers','cleanCardServices']);
+var cleanCard = angular.module('cleanCard', ['ngRoute', 'cleanCardControllers', 'cleanCardServices']);
+
+
+cleanCard.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      when('/sessions/new', {
+      	templateUrl: 'sessions/new.html',
+      	controller: 'ItemsCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+}]);
